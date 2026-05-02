f843b800fb14629dbe1f44fd5e1f8fe6079fee34
Zomikan <49733297+Zomikan@users.noreply.github.com>
2026-05-03 01:20:06 +0200

    Update asbestos_app.py

--- asbestos_app.py
+++ asbestos_app.py
@@ -32,8 +32,7 @@ def generate_pdf(project_info, items, total):
     buffer = io.BytesIO()
     c = canvas.Canvas(buffer, pagesize=letter)
     width, height = letter
-
-	c.drawImage("logo.png", 1*inch, height - 1.6*inch, 
+    c.drawImage("logo.png", 1*inch, height - 1.5*inch, 
 	width=2*inch, preserveAspectRatio=True)
 
     c.setFont("Helvetica-Bold", 16)
